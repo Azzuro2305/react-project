07568fae4c1f9877760f57e752141390e37e53cf
Chwen <zzero.i.n.g@gmail.com>
2024-02-12 16:00:29 +0630

    re commit

--- vite-project/src/pages/Aboutus.jsx
+++ vite-project/src/pages/Aboutus.jsx
@@ -1,11 +1,103 @@
 import React from 'react'
+import Profile from '../components/Profile';
+import Images from '../components/Images';
+// import image from '../assets/cleaning1.png'
+import Button from '../components/Button';
 
-function Aboutus() {
-  return (
-    <>
-        About us
-    </>
-  )
+const Aboutus = () => {
+    const profiles = [
+        // {
+        //     id: 1,
+        //     name: "Jennifer",
+        //     position: "Founder & CEO",
+        //     myImage: Images.profile2,
+        //     text: "Ms. Jennifer is the young entrepreneur with more than 11 years of business experiences in facility management. Her Management, Strategic Planning,Leadership and Team Control is the major key of the Clean Pro’s Success Stories."
+        // },   
+        {
+          id: 3,
+            name: "Andrew",
+            position: "Operation Manager",
+            myImage: Images.profile3,
+            text: "Mr.Andrew strong knowledge and skills on Housekeeping operations in Hotels, Hospitals and condominiums are being utilized to promote systematic, effective, and safe operations in cleaning."
+        },
+        {
+            id: 2,
+            name: "Asheley",
+            position: "Deputy CEO",
+            myImage: Images.profile1,
+            text: "Ms. Ashley is the young entrepreneur with more than 9 years of business experiences in facility management."
+        },
+        {
+          id: 4,
+            name: "Mark",
+            position: "Operation Manager",
+            myImage: Images.profile4,
+            text: "Mr.Mark the Daily Cleaning Operation has very rich experience in Construction Cleaning, Floor Maintenance & Crystallization, and Deep Cleaning Operations."
+        },
+    ]
+    return (
+        <div>
+            <h1 className='bg-#35D145 text-center py-40px font-lato text-5xl text-white uppercase font-bold'>About Us</h1>
+
+            <section className='container m-auto my-14 font-poppins'>
+                <div className="flex">
+                    <div className="w-1/2">
+                        <h3 className='mb-5 font-semibold text-4xl'>Welcome to GLC</h3>
+
+                        <p className='pr-5 mb-5'>Turpis adipiscing netus est auctor. Pellentesque et tincidunt varius enim pulvinar miodio. Elementum pellentesque facilisis bibendum pharetra aliquet nascetur. Malesuada erat suspendisse risus turpis ullamcorper adipiscing quisque viverra nulla.
+                        Pellentesque et tincidunt varius enim pulvinar miodio. Elementum pellentesque facilisis bibendum pharetra aliquet nascetur.</p>
+                            
+                        <p className='pr-5 mb-5'>Vel odio tristique faucibus. Dolor sed nisl consequat etiam. Ultricies in in sed fermentum urna.Mattis purus hendrerit sed eget. Neque cursus nisl consequat tincidunt.Mattis purus hendrerit sed eget. Neque cursus nisl consequat tincidunt. </p>
+
+                        <p className='pr-5'>Mattis purus hendrerit sed eget. Neque cursus nisl consequat tincidunt.Vel odio tristique faucibus. Dolor sed nisl consequat etiam. Ultricies in in sed fermentum urna. 
+                        Elementum pellentesque facilisis bibendum pharetra aliquet nascetur. </p>
+                    </div>
+
+                    <div className="w-1/2">
+                        <img className='ms-auto' src={Images.room2} alt="Room Image" />
+                    </div>
+                </div>
+
+                <div className='flex mb-8'>
+                    <div className="flex border-l-8 p-4 border-#35D145 me-40">
+                        <span className='text-6xl font-bold me-4 text-#35D145'>18</span> 
+                        <div className='mt-1.5 font-semibold'>
+                            <p className='text-gray-500'>Years Of</p> 
+                            <p className='uppercase text-2xl'>experience</p>
+                        </div>
+                    </div>
+
+                    <div className="flex border-l-8 p-4 border-#35D145">
+                        <span className='text-6xl font-bold me-4 text-#35D145'>50</span> 
+                        <div className='mt-1.5 font-semibold'>
+                            <p className='text-gray-500'>Popular</p> 
+                            <p className='uppercase text-2xl'>Master Cleaners</p>
+                        </div>
+                    </div>
+                </div>
+
+                <Button txt="Read More"/>
+            </section>
+
+            <section className='bg-green-200 text-center py-14'>
+                <h2 className='uppercase text-5xl font-bold mb-20'>Our Management Team</h2>
+                <div className='profiles container m-auto'>
+                    <div className='flex justify-center mb-1'>
+                        {  
+                            <Profile key="1" img={Images.profile2} name="Jennifer" rank="Founder & CEO" txt= "Ms. Jennifer is the young entrepreneur with more than 11 years of business experiences in facility management. Her Management, Strategic Planning,Leadership and Team Control is the major key of the Clean Pro’s Success Stories." />
+                        }
+                    </div>
+                    <div className='flex flex-wrap justify-between'>
+                        {
+                            profiles.map((profile)=>
+                            <Profile key={profile.id} img={profile.myImage} name={profile.name} rank={profile.position} txt={profile.text} />
+                            )
+                        }
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Aboutus
--- vite-project/src/pages/Qa.jsx
+++ vite-project/src/pages/Qa.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Images from '../components/Images'
 
 function Qa() {
   return (
     <>
-      Qa
+    <div className='bg-#35D145 text-center py-40px font-lato text-5xl text-white uppercase font-bold'>Quality Assurance</div>
+
+    <div className='bg-green-300'>
+      <img src={Images.room3} alt="QA foto" />  
+      <div className="container m-auto py-16">
+        <h3 className='text-#27AC35 font-lato font-extrabold text-3xl mb-10'>How we ensure quality</h3>
+
+        <p className='mb-6'>Lorem ipsum dolor sit amet consectetur. Commodo arcu malesuada lacus et vel tristique sagittis neque. Urna nulla eleifend turpis sollicitudin tempor. Ipsum mattis iaculis facilisis aliquet non id nec ipsum. Purus ac leo egestas dolor nunc. Commodo justo lectus aliquam morbi malesuada ornare dignissim. Elementum in ac luctus tellus id consectetur maecenas. Nisl nec sapien lobortis commodo. Tristique lobortis sit orci nam dictum vel vitae eget id. </p>
+
+        <p className='mb-6'>Ultrices eu sed sed ultrices erat. Consequat tortor bibendum eget orci ullamcorper est est. Eu eu varius consequat in porta venenatis est quis ipsum. Id tellus at ac sed aenean bibendum augue sem pulvinar. Placerat ultricies aliquam interdum dis. Tellus massa nisi fermentum ut interdum quis quis in tellus. Ultricies viverra metus purus massa. Arcu auctor at ipsum volutpat.</p>
+
+        <p className='mb-6'>Nunc lacinia diam magnis nisl suscipit quis. Habitant cursus nulla tempus orci. Dolor urna maecenas egestas pellentesque. Scelerisque pulvinar morbi habitant odio vitae tellus accumsan. Et malesuada sodales aliquet amet pellentesque nulla. Commodo sed mi diam magna iaculis ac. A mauris fringilla lectus tempor lobortis. </p>
+
+        <p className='mb-6'>In turpis tellus sagittis ut accumsan vulputate a nisi eu. Nunc odio tincidunt adipiscing molestie dictumst vel velit molestie. Augue tincidunt aliquam nec accumsan. </p>
+
+        <p className='mb-6'>Vestibulum at ut posuere morbi euismod ac integer. In proin nullam odio sed a et. Ipsum euismod dolor nascetur nec hendrerit amet. Adipiscing amet elit adipiscing platea at risus egestas magna tortor. Morbi ornare quam cras dignissim lorem quis. Eros aliquam ac ornare dignissim ultricies. Vulputate arcu diam fringilla gravida tellus magna habitasse. </p>
+
+        <p className='mb-6'>Ut platea tellus integer integer non semper tempor in sed. Eget nunc enim etiam velit adipiscing vitae ornare et. Volutpat aliquam pellentesque orci pharetra faucibus morbi massa nulla. Et augue est vitae fermentum ac rhoncus turpis orci mi. </p>
+
+        <p className='mb-6'>Diam hac vehicula nibh consequat placerat maecenas ut fermentum. Nec eget ac senectus felis feugiat in vel at fames. Vel turpis ultricies tristique egestas purus. Erat fringilla non in urna adipiscing molestie dictum tortor. Dictum pretium eu amet urna. Id proin mi morbi dictumst mauris tincidunt tristique eget. </p>
+
+        <p className='mb-6'>Id congue aliquet egestas lacus dignissim integer augue suspendisse. Odio et et at posuere fringilla ut. Neque platea fermentum blandit sapien malesuada arcu eget in et. Tellus donec placerat eu ante bibendum venenatis. </p>
+
+        <p className='mb-6'>Turpis purus suspendisse tortor tellus nam pharetra id viverra. Tempus massa iaculis enim tincidunt.</p>
+
+        <p className='mb-6'>Ut platea tellus integer integer non semper tempor in sed. Eget nunc enim etiam velit adipiscing vitae ornare et. Volutpat aliquam pellentesque orci pharetra faucibus morbi massa nulla. Et augue est vitae fermentum ac rhoncus turpis orci mi. </p>
+
+        <p className='mb-6'>Diam hac vehicula nibh consequat placerat maecenas ut fermentum. Nec eget ac senectus felis feugiat in vel at fames. Vel turpis ultricies tristique egestas purus. Erat fringilla non in urna adipiscing molestie dictum tortor. Dictum pretium eu amet urna. Id proin mi morbi dictumst mauris tincidunt tristique eget. </p>
+
+      </div>
+
+    </div> 
     </>
+
   )
 }
 
